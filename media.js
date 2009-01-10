37d7ec3347fc39922cc273d6eb114068b30c6c9e
Arthur Foelsche <arthurf@27259.no-reply.drupal.org>
2009-01-10 02:35:34 +0000

    Got some mockup js and css working for the browser

--- media.js
+++ media.js
@@ -7,9 +7,36 @@
  
  
  $(document).ready( function () { 
-   // get the list of form elements that need to be hidden
  
+  // hide the display information on page load
+  $('.media.browser.display').each(function() {
+    $(this).hide();
+  });
+ 
+   // Activate drawers and displays when the page loads
+  $('.ui-tabs-panel').each(function () { 
+	  // set the first drawer active
+	  $(this).children('.media.browser.drawer:first').addClass('active');
+	  // show the display of the first drawer
+	  $(this).children('.media.browser.display:first').addClass('active').show();
+	});
+	
+	// now we need to bind click functionality on drawers to display
+	$('.media.browser.drawer').click( function () {
+	  // constrain the click to inside this tab
+	  
+	  // get parent
+	  var parent = $(this).parent('div');
+	  // remove active class from current drawer
+	  $(parent).children('.media.browser.drawer.active').removeClass('active');
+	  // remove active class from current display
+	  $(parent).children('.media.browser.display.active').removeClass('active').hide();
+	  // set this drawer active
+	  $(this).addClass('active');
+	  // make the display for this drawer active
+	  $(this).next('.media.browser.display').addClass('active').show();
+	});
 
- 
+
  });
  
