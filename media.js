772efee13c752b6ca9d866081030857014d392f6
Arthur Foelsche <arthurf@27259.no-reply.drupal.org>
2009-01-10 07:43:05 +0000

    fixes some of the javascript issues

--- media.js
+++ media.js
@@ -6,37 +6,50 @@
  */
  
  
+ /**
+  * This handles the activation of drawers on the media browser form
+  * @TODO make this a drupal behavior
+  */
  $(document).ready( function () { 
- 
   // hide the display information on page load
   $('.media.browser.display').each(function() {
     $(this).hide();
   });
  
-   // Activate drawers and displays when the page loads
-  $('.ui-tabs-panel').each(function () { 
-	  // set the first drawer active
-	  $(this).children('.media.browser.drawer:first').addClass('active');
-	  // show the display of the first drawer
-	  $(this).children('.media.browser.display:first').addClass('active').show();
-	});
-	
+  // Activate drawers when the page loads  
+  $('.media.browser .ui-tabs-panel .drawers .item-list li.first').addClass('active');
+  // Activate displays when the page loads  
+  $('.media.browser .ui-tabs-panel .display.first').addClass('active').show();
+  
+  
 	// now we need to bind click functionality on drawers to display
-	$('.media.browser.drawer').click( function () {
-	  // constrain the click to inside this tab
+	$('.media.browser .ui-tabs-panel .drawers .item-list ul li').bind('click', function () {
+	  // get the href id that we want to display 
+	  var display_id = $(this).children('a').attr('href');
+	  // we need to get the container that this drawer is in
+	  var parent = $(this).parents('.ui-tabs-panel').attr('id');
 	  
-	  // get parent
-	  var parent = $(this).parent('div');
-	  // remove active class from current drawer
-	  $(parent).children('.media.browser.drawer.active').removeClass('active');
-	  // remove active class from current display
-	  $(parent).children('.media.browser.display.active').removeClass('active').hide();
-	  // set this drawer active
+	  // hide current active display
+	  $('#'+parent+' .display.active').removeClass('active').hide();
+	  // set any drawers to not active
+	  $('#'+parent+' .drawers li.active').removeClass('active');
+	  // make this drawer active
 	  $(this).addClass('active');
-	  // make the display for this drawer active
-	  $(this).next('.media.browser.display').addClass('active').show();
-	});
-
-
+	  // make the correct display active
+	  $(display_id).addClass('active').show();
+   });
+   
  });
+ 
+ 
+ /**
+  * This handles passing the current file data from the media browser
+  * to the formater function and returns the correct form elements
+  */
+$(document).ready(function () {
+ 
+ 
+});
+ 
+  
  
