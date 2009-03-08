b3140d80d0df0fc45497f2c59635b91758d21e07
Arthur Foelsche <arthurf@27259.no-reply.drupal.org>
2009-03-08 21:30:17 +0000

    * Significant simplification of the form building process for the browser * Added a form display theme to make the panel of the media browser easier to deal with * improved the css to deal with the new structure * made the JS more generic and adaptive to the changes in the form structure * a few changes to the media mover module integration

--- javascript/media.js
+++ javascript/media.js
@@ -7,39 +7,44 @@
  
  
  /**
-  * This handles the activation of drawers on the media browser form
+  * This handles the display, activation and
+  * hiding  of drawers on the media browser form
   * @TODO make this a drupal behavior
   */
- $(document).ready( function () { 
-  // hide the display information on page load
-  $('.media.browser.display').each(function() {
+ $(document).ready( function () {
+   
+  // hide all the drawer display items on page load
+  $('.media.browser .ui-tabs-panel .drawer.display').each(function() {
     $(this).hide();
   });
- 
-  // Activate drawers when the page loads  
-  $('.media.browser .ui-tabs-panel .drawers .item-list li.first').addClass('active');
-  // Activate displays when the page loads  
-  $('.media.browser .ui-tabs-panel .display.first').addClass('active').show();
+
+  // activate all the drawer data display that needs to be show when 
+  // the page is loaded
+  $('.media.browser .ui-tabs-panel .drawer.display.active').each(function() {
+    $(this).show();
+  });
+
+  // --------------------------------------
+  // click actions
   
-  
-	// now we need to bind click functionality on drawers to display
-	$('.media.browser .ui-tabs-panel .drawers .item-list ul li, .drawers li a').bind('click', function () {
-	  // get the href id that we want to display 
-	  // is this an a element? 
-	  var display_id = $(this).attr('href');
-	  if (display_id == undefined) {
-	    var display_id = $(this).children('a').attr('href');
-	  }
-	  // we need to get the container that this drawer is in
-	  var parent = $(this).parents('.ui-tabs-panel').attr('id');	  
-	  // hide current active display
-	  $('#'+parent+' .display.active').removeClass('active').hide();
-	  // set any drawers to not active
-	  $('#'+parent+' .drawers li.active').removeClass('active');
-	  // make this drawer active
-	  $(this).addClass('active');
-	  // make the correct display active
-	  $(display_id).addClass('active').show();
+  // now we need to bind click functionality on drawers to display
+  $('.media.browser .drawers .item-list ul li, .drawers li a').bind('click', function () {
+    // get the href id that we want to display 
+    var display_id = $(this).attr('href');
+    // this handles the LI click
+    if (display_id == undefined) {
+      var display_id = $(this).children('a').attr('href');
+    }
+    // we need to get the tab page that this drawer is in
+    var parent = $(this).parents('.ui-tabs-panel').attr('id');
+    // hide current active display
+    $('#'+parent+' .display.active').removeClass('active').hide();
+    // set any drawers to not active
+    $('#'+parent+' .drawers li.active').removeClass('active');
+    // make this drawer active
+    $(this).addClass('active');
+    // make the correct display active
+    $(display_id).addClass('active').show();
    });
    
  });
@@ -50,18 +55,15 @@
   * activate the add button, and hide the browser
   */
  $(document).ready(function () {
-   $('.media.replace').hide();
+   $('.media.browser.wrapper').hide();
    $('.media.browser.activation').each(function () {
      $(this).next('.media.browser').hide();
      $(this).click(function () {
        $(this).next('.media.browser').slideDown('slow');
        $(this).slideUp();
      });
-   }); 
-   
+   });   
  });
- 
- 
  
  
  /**
