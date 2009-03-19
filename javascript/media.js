757afa47a2253793d1099885aaf9a39d0f0353ed
aaronwinborn <aaron@aaronwinborn.com>
2009-03-19 01:16:10 +0000

    * Drupal.behaviors.mediaGenerateMD5 (aaron).

--- javascript/media.js
+++ javascript/media.js
@@ -1,24 +1,24 @@
 // $Id$
 
 /**
- * @file 
+ * @file
  * This file handles the JS for Media Module functions
  */
- 
- 
+
+
  /**
   * This handles the display, activation and
   * hiding  of drawers on the media browser form
   * @TODO make this a drupal behavior
   */
  $(document).ready( function () {
-   
+
   // hide all the drawer display items on page load
   $('.media.browser .ui-tabs-panel .drawer.display').each(function() {
     $(this).hide();
   });
 
-  // activate all the drawer data display that needs to be show when 
+  // activate all the drawer data display that needs to be show when
   // the page is loaded
   $('.media.browser .ui-tabs-panel .drawer.display.active').each(function() {
     $(this).show();
@@ -26,10 +26,10 @@
 
   // --------------------------------------
   // click actions
-  
+
   // now we need to bind click functionality on drawers to display
   $('.media.browser .drawers .item-list ul li, .drawers li a').bind('click', function () {
-    // get the href id that we want to display 
+    // get the href id that we want to display
     var display_id = $(this).attr('href');
     // this handles the LI click
     if (display_id == undefined) {
@@ -46,10 +46,10 @@
     // make the requested drawer display active
     $(display_id).addClass('active').show();
    });
-   
+
  });
- 
- 
+
+
  /**
   * we need to hide any form elements that were replaced by the media browser form
   * activate the add button, and hide the browser
@@ -62,22 +62,18 @@
        $(this).next('.media.browser').slideDown('slow');
        $(this).slideUp();
      });
-   });   
+   });
  });
- 
- 
- /**
-  * Generate a MD5 hash of the file being uploaded
-  */
-$(document).ready(function () {
-  // get the value from the file field 
-  $('#edit-field-file-media-media-tabs-tab-Addfiles-media-upload-resource-Newfile-resource-form-media-upload-upload').change(function () {
-    // Add the MD5 hash from the file name to the upload URL
+
+/**
+ * Generate a MD5 hash of the file being uploaded
+ */
+Drupal.behaviors.mediaGenerateMD5 = function(context) {
+  // Get the value from the file field.
+  $('#edit-field-file-media-media-tabs-tab-Addfiles-media-upload-resource-Newfile-resource-form-media-upload-upload:not(.mediaGenerateMD5-processed)', context).addClass('mediaGenerateMD5-processed').change(function () {
+    // Add the MD5 hash from the file name to the upload URL.
     Drupal.settings['ahah']['edit-attach']['url'] += '/'+$.md5($(this).val());
-    // @TODO now add the MD5 value to the meta data form    
-    alert(Drupal.settings['ahah']['edit-attach']['url']);    
+    // @TODO: Now add the MD5 value to the meta data form.
+    alert(Drupal.settings['ahah']['edit-attach']['url']);
   });
-});
- 
-  
- 
+};
